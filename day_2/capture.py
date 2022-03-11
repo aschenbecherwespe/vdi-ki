4f6b88100f75ce09e3dfed4f911d74bade495db3
aschenbecherwespe <94011659+aschenbecherwespe@users.noreply.github.com>
2022-03-11 13:53:33 +0100

    add capture script

---
+++ day_2/capture.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# Normally the QtGlPreview implementation is recommended as it benefits
+# from GPU hardware acceleration.
+
+from qt_gl_preview import *
+from picamera2 import *
+import time
+
+picam2 = Picamera2()
+preview = QtGlPreview(picam2)
+
+preview_config = picam2.preview_configuration()
+picam2.configure(preview_config)
+
+capture_config = picam2.still_configuration()
+picam2.configure(capture_config)
+
+picam2.start()
+
+# load image
+
+while True:
+    input("press return to trigger")
+    image = picam2.switch_mode_and_capture_image(capture_config)
+    #do something with the image
+
+
+
+
